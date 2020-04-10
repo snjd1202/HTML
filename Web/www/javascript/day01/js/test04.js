5212e1736cf3557fd4a233b63f81b25d50f0be81
class02 <class02@DESKTOP-4772329>
2020-04-10 18:13:31 +0900

    4/10 commit

---
+++ Web/www/javascript/day01/js/test04.js
@@ -0,0 +1,6 @@
+document.write(
+	'<div class="w3-col m3"><p></p></div>' +
+	'<div class="w3-col m6 w3-center">' + 
+		'<h2 class="w3-padding w3-red w3-text-white">현우씨!<br>공부 좀 하자!</h2>' +
+	'</div>'
+);
